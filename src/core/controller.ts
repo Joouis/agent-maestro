02aff90d162a3208d1ad1332dd88a9017f9c834d
John Chou <luckyjoou@gmail.com>
2025-07-03 12:20:25 +0800

    Update

--- src/core/controller.ts
+++ src/core/controller.ts
@@ -3,7 +3,11 @@ import * as vscode from "vscode";
 import { logger } from "../utils/logger";
 import { ClineAdapter } from "./ClineAdapter";
 import { RooCodeAdapter } from "./RooCodeAdapter";
-import { AgentMaestroConfiguration, readConfiguration } from "../utils/config";
+import {
+  AgentMaestroConfiguration,
+  DEFAULT_CONFIG,
+  readConfiguration,
+} from "../utils/config";
 import { ExtensionStatus } from "../utils/systemInfo";
 
 /**
@@ -40,7 +44,7 @@ export class ExtensionController extends EventEmitter {
     // Check and create adapters for each variant identifier
     for (const identifier of new Set([
       ...config.rooVariantIdentifiers,
-      "rooveterinaryinc.roo-cline",
+      DEFAULT_CONFIG.defaultRooIdentifier,
     ])) {
       if (
         identifier !== config.defaultRooIdentifier &&
